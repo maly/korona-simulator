57269e33622e6410d5d5d8ccd9c81c5d42588530
Martin Maly <maly@maly.cz>
2020-03-17 22:30:54 +0100

    minify

--- sim.js
+++ sim.js
@@ -1,5 +1,5 @@
 window.$ = window.jQuery = require("./node_modules/jquery/dist/jquery.min.js");
-require("./node_modules/bootstrap/dist/js/bootstrap.min");
+//require("./node_modules/bootstrap/dist/js/bootstrap.min");
 
 var context;
 var gctx;
